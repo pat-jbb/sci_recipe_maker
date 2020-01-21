a8233277661370480ee4401cfff646fa5dce03a9
taphrednif <T@phr3dn1f>
2020-01-21 22:15:47 +0800

    Added snippets templates

--- models/recipe.py
+++ models/recipe.py
@@ -34,16 +34,7 @@ class RecipeRecipe(models.Model):
 
     notes = fields.Html("Recipe Notes", translate=True)
     image_caption = fields.Char("Image Caption")
-    image = fields.Binary("Image", attachment=True,
-                          help="This field holds the image used as avatar for this recipe, limited to 1024x1024px", )
-    image_medium = fields.Binary("Medium-sized image", attachment=True,
-                                 help="Medium-sized image of this contact. It is automatically " \
-                                      "resized as a 128x128px image, with aspect ratio preserved. " \
-                                      "Use this field in form views or some kanban views.")
-    image_small = fields.Binary("Small-sized image", attachment=True,
-                                help="Small-sized image of this contact. It is automatically " \
-                                     "resized as a 64x64px image, with aspect ratio preserved. " \
-                                     "Use this field anywhere a small image is required.")
+    image_medium = fields.Binary("Recipe image", attachment=True)
 
     @api.model
     def _get_published_date(self):
